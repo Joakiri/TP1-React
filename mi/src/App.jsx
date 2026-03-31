e78a306cddf2be0d76e6d3a3c914caa497560daf
JOAQUÍN IRIBAS <49627045@ort>
2026-03-31 10:26:53 -0300

    EstaBien, Hasta aqui llegue

--- mi/src/App.jsx
+++ mi/src/App.jsx
@@ -1,24 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
 import Header from './components/Header'
 import Formulario from './components/Formulario'
 import Listado from './components/Listado'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() {  
 
   return (
     <>
     <Header />
     <div className="container">
-      <div className="left">
-        <Formulario />
-      </div>
-      <div className="right">
-        <Listado />
+      <div class="row">
+        <div className="one-half column">
+          <Formulario />
+        </div>
+        <div className="one-half column">
+          <Listado />
+        </div>
       </div>
     </div>
     </>
