32cff92fc8cf91bdd794c3940f0c4b685ed0607e
JOAQUÍN IRIBAS <49627045@ort>
2026-03-31 10:05:48 -0300

    lol

---
+++ mi/src/components/Listado.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Cita from './Cita'
+
+export default function Listado() {
+  return (
+    <div>
+        <h2>Administra tus citas</h2>
+        <Cita/>
+    </div>
+  )
+}
